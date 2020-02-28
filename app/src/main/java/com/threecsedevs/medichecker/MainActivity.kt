321d12928b5f7fbe14199c9842771a39bf69a70f
Taewan-P <swimingtw@gmail.com>
2020-02-28 22:13:11 +0900

    Add test fragment
    not working

--- app/src/main/java/com/threecsedevs/medichecker/MainActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.threecsedevs.medichecker
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import me.ibrahimsn.lib.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomBar.setOnItemSelectedListener(object: OnItemSelectedListener {
+            override fun onItemSelect(pos: Int) {
+                when(pos) {
+                    0 -> {
+                        val fragment = HomeActivity()
+                        supportFragmentManager.beginTransaction().replace(R.id.home, fragment, fragment.javaClass.simpleName).commit()
+                    }
+                    1 -> {
+                        val fragment = SearchActivity()
+                        supportFragmentManager.beginTransaction().replace(R.id.search, fragment, fragment.javaClass.simpleName).commit()
+                    }
+                    2 -> {
+                        val fragment = ProfileActivity()
+                        supportFragmentManager.beginTransaction().replace(R.id.profile, fragment, fragment.javaClass.simpleName).commit()
+                    }
+                    else -> {
+                        val fragment = SearchActivity()
+                        supportFragmentManager.beginTransaction().replace(R.id.search, fragment, fragment.javaClass.simpleName).commit()
+                    }
+
+                }
+
+            }
+        })
     }
+
+
 }
---
+++ app/src/main/java/com/threecsedevs/medichecker/ProfileActivity.kt
@@ -0,0 +1,21 @@
+package com.threecsedevs.medichecker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProfileActivity : Fragment() {
+
+    fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
+        return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return container?.inflate(R.layout.activity_profile)
+    }
+}
