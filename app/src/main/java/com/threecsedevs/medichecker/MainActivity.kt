6102d7d6490040293b0fe3ce9f5b2374d7c44934
Taewan-P <swimingtw@gmail.com>
2020-02-29 14:20:05 +0900

    Refactor id for FrameLayout

--- app/src/main/java/com/threecsedevs/medichecker/MainActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/MainActivity.kt
@@ -11,25 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var fragment = SearchActivity()
-        supportFragmentManager.beginTransaction().replace(R.id.HomePage, fragment, fragment.javaClass.simpleName).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.mainPage, fragment, fragment.javaClass.simpleName).commit()
         bottomBar.setOnItemSelectedListener(object: OnItemSelectedListener {
             override fun onItemSelect(pos: Int) {
                 when(pos) {
                     0 -> {
                         val fragment = HomeActivity()
-                        supportFragmentManager.beginTransaction().replace(R.id.HomePage, fragment, fragment.javaClass.simpleName).commit()
+                        supportFragmentManager.beginTransaction().replace(R.id.mainPage, fragment, fragment.javaClass.simpleName).commit()
                     }
                     1 -> {
                         val fragment = SearchActivity()
-                        supportFragmentManager.beginTransaction().replace(R.id.HomePage, fragment, fragment.javaClass.simpleName).commit()
+                        supportFragmentManager.beginTransaction().replace(R.id.mainPage, fragment, fragment.javaClass.simpleName).commit()
                     }
                     2 -> {
                         val fragment = ProfileActivity()
-                        supportFragmentManager.beginTransaction().replace(R.id.HomePage, fragment, fragment.javaClass.simpleName).commit()
+                        supportFragmentManager.beginTransaction().replace(R.id.mainPage, fragment, fragment.javaClass.simpleName).commit()
                     }
                     else -> {
                         val fragment = SearchActivity()
-                        supportFragmentManager.beginTransaction().replace(R.id.HomePage, fragment, fragment.javaClass.simpleName).commit()
+                        supportFragmentManager.beginTransaction().replace(R.id.mainPage, fragment, fragment.javaClass.simpleName).commit()
                     }
 
                 }
