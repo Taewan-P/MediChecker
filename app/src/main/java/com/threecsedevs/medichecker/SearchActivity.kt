3b6bfb47665ddd3236acd5a7c43c416c3f6cee8d
Taewan-P <swimingtw@gmail.com>
2020-03-02 01:05:27 +0900

    Remove unnecessary imports

--- app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
@@ -1,12 +1,12 @@
 package com.threecsedevs.medichecker
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-//import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
