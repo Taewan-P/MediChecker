c717acd5a8e2508c80d1f52cf1f03330dec1db97
Taewan-P <swimingtw@gmail.com>
2020-03-03 20:39:06 +0900

    Add Buttons with Add Button

--- app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
@@ -1,15 +1,14 @@
 package com.threecsedevs.medichecker
 
+import android.app.ActionBar
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Toast
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_search.*
 
@@ -26,6 +25,7 @@ class SearchActivity : Fragment() {
         return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,61 +37,26 @@ class SearchActivity : Fragment() {
     // After the view is created
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        var word = "Tami"
-        testButton.setOnClickListener {
-            var queue = Volley.newRequestQueue(this.context!!)
-            val url = "https://rxnav.nlm.nih.gov/REST/spellingsuggestions.json?name="
-
-            val name = "Tami"
-            val stringrequest = object :
-                StringRequest(Request.Method.GET, url + name, Response.Listener { response ->
-                    println("Receiving Response : $response")
-                    testText.text = response
-
-                }, Response.ErrorListener { error ->
-                    Log.d("ERROR", "Response Unsuccessful : $error")
-                }) {
-                    override fun getBodyContentType(): String {
-                        return "application/json; charset=utf-8"
-                    }
-//                    override fun getBody(): ByteArray {
-////                        return requestBody.toByteArray()
-////                    }
-
+        var counter: Int = 1;
+        addBtn.setOnClickListener {
+            val newBtn : Button = Button(this.context)
+            val params : LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            newBtn.id = counter
+            newBtn.layoutParams = params
+            newBtn.text = "Input Medicine"
+            newBtn.setOnClickListener {
+                Toast.makeText(this.context, newBtn.text,Toast.LENGTH_SHORT).show()
             }
-            Volley.newRequestQueue(context).add(stringrequest)
+            parentlayout.addView(newBtn)
+            counter ++
+
+
 
         }
 
-//            val suggestions = SpellingCheck.getSpellingSuggestions(this.context!!, word) { testSuccess ->
-//                if (testSuccess) {
-//                    Toast.makeText(this.context!!, "Success!!!", Toast.LENGTH_SHORT).show()
-//                }
-//                else {
-//                    Toast.makeText(this.context!!, "Failed...", Toast.LENGTH_SHORT).show()
-//                }
-//
-//            }
-
-
-        val drugList = arrayOf(
-            "Acetaminophen", "Acetadryl", "Acetazolamide",
-            "Acetaminophen-codeine", "INFANTS' Acetaminophen")
-
-        val adapter = ArrayAdapter<String>(
-            this.context!!, android.R.layout.simple_dropdown_item_1line, drugList)
-
-        drugInput.setAdapter(adapter)
-        drugInput.threshold = 3
-
-        // When Suggestion is clicked
-        drugInput.onItemClickListener = AdapterView.OnItemClickListener{
-                parent, view, position, id->
-            val selectedItem = parent.getItemAtPosition(position).toString()
-            // Display the clicked item using toast
-            Toast.makeText(activity?.applicationContext,"Selected : $selectedItem",Toast.LENGTH_SHORT).show()
-        }
 
     }
 
