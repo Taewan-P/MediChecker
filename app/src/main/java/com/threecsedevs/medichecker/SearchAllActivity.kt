b828004d104cace45e4b33b0485267d61a2872aa
Taewan-P <swimingtw@gmail.com>
2020-03-04 23:26:37 +0900

    Add searchall activity with layout

---
+++ app/src/main/java/com/threecsedevs/medichecker/SearchAllActivity.kt
@@ -0,0 +1,11 @@
+package com.threecsedevs.medichecker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchAllActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_popup)
+    }
+}
