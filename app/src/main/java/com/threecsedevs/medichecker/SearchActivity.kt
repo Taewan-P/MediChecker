e5da24b243d7a5ea49bf7ba76a398bfe2e4ce56b
Yeonggyun.Kim <kygha7205@naver.com>
2020-03-04 23:12:48 +0900

    Button delete test code added

--- app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
@@ -1,23 +1,12 @@
 package com.threecsedevs.medichecker
 
-import android.app.ActionBar
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_search.*
-
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import org.json.JSONArray
-import org.json.JSONObject
 
 class SearchActivity : Fragment() {
 
@@ -56,6 +45,12 @@ class SearchActivity : Fragment() {
             counter ++
 
         }
+        deleteBtn.setOnClickListener {
+//            val tmpbtn = counter as Button
+            parentlayout.removeView(parentlayout.getChildAt(parentlayout.childCount-1))
+            counter --
+        }
+
 
 
     }
