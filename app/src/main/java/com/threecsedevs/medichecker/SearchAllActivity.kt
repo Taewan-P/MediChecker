69cf163310583ac0e117852db4d4e79026d19e71
Taewan-P <swimingtw@gmail.com>
2020-03-05 20:03:20 +0900

    Add test SearchAllActivity

--- app/src/main/java/com/threecsedevs/medichecker/SearchAllActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/SearchAllActivity.kt
@@ -1,11 +1,26 @@
 package com.threecsedevs.medichecker
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search_popup.*
 
 class SearchAllActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_popup)
+
+
+        testSubmit.setOnClickListener {
+            val text = testInput.text
+            val intent = Intent()
+            intent.putExtra("medicine", text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
     }
 }
