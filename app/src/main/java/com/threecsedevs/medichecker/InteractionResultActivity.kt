25e29f6309336962711a89adf50e36dbce452e56
Taewan-P <swimingtw@gmail.com>
2020-03-15 22:43:34 +0900

    Add loading circle
    but not working yet.

--- app/src/main/java/com/threecsedevs/medichecker/InteractionResultActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/InteractionResultActivity.kt
@@ -1,6 +1,7 @@
 package com.threecsedevs.medichecker
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_interaction_result.*
 
@@ -8,8 +9,10 @@ class InteractionResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interaction_result)
+        loadingCircle.visibility = View.VISIBLE
         var result = intent.getStringExtra("result")
         println("result at InteractionResultActivity : $result")
+        loadingCircle.visibility = View.GONE
         resultTest.text = result
     }
 }
