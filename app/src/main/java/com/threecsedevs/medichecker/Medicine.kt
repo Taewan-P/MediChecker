a2deb2a8b1432c23850a629a01ef8d43c9f1770c
Taewan-P <swimingtw@gmail.com>
2020-03-17 02:13:56 +0900

    Update Medicine class
    mistake

--- app/src/main/java/com/threecsedevs/medichecker/Medicine.kt
+++ app/src/main/java/com/threecsedevs/medichecker/Medicine.kt
@@ -1,9 +1,8 @@
 package com.threecsedevs.medichecker
 
-class Medicine {
-    var name : String? = null
-    var morning : Boolean = false
-    var lunch : Boolean = false
+class Medicine (
+    var name : String? = null,
+    var morning : Boolean = false,
+    var lunch : Boolean = false,
     var dinner : Boolean = false
-
-}
+)
