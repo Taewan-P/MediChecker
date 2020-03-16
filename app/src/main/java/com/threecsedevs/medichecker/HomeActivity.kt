86653da6bbb9a72e493b075b17ba81eff84fabf8
Taewan-P <swimingtw@gmail.com>
2020-03-17 02:14:19 +0900

    Inflate CustomListView to HomeActivity

--- app/src/main/java/com/threecsedevs/medichecker/HomeActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/HomeActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : Fragment() {
 
@@ -17,5 +19,15 @@ class HomeActivity : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return container?.inflate(R.layout.activity_home)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var mlist = mutableListOf<Medicine>()
+
+        mlist.add(Medicine("Tyrenol", morning = true, lunch = true, dinner = true))
+        mlist.add(Medicine("Tamiflu", morning = true, lunch = false, dinner = true))
+        mlist.add(Medicine("Ibuprophen", morning = false, lunch = false, dinner = false))
+
+        medicineList.adapter = ListViewAdapter(this.context!!, R.layout.medicine_item, mlist)
+
     }
 }
