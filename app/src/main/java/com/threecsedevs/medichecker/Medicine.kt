c828d17163019a0ea9b109005e256b9f8b408702
Taewan-P <swimingtw@gmail.com>
2020-03-17 01:31:57 +0900

    Add Medicine Class for ListView

--- app/src/main/java/com/threecsedevs/medichecker/Medicine.kt
+++ app/src/main/java/com/threecsedevs/medichecker/Medicine.kt
@@ -1,3 +1,9 @@
 package com.threecsedevs.medichecker
 
+class Medicine {
+    var name : String? = null
+    var morning : Boolean = false
+    var lunch : Boolean = false
+    var dinner : Boolean = false
 
+}
