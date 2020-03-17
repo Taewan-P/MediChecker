1a18a6b5ba478cf8ded218301b6c35a135123c28
Taewan-P <swimingtw@gmail.com>
2020-03-17 15:11:07 +0900

    Add custom dialog box to add medicine

--- app/src/main/java/com/threecsedevs/medichecker/HomeActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/HomeActivity.kt
@@ -1,9 +1,12 @@
 package com.threecsedevs.medichecker
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -27,6 +30,29 @@ class HomeActivity : Fragment() {
         mlist.add(Medicine("Ibuprophen", morning = false, lunch = false, dinner = false))
 
         medicineList.adapter = MedicineListViewAdapter(this.context!!, R.layout.medicine_item, mlist)
+
+        add_button.setOnClickListener {
+            val builder = AlertDialog.Builder(this.context)
+            val dialogView = layoutInflater.inflate(R.layout.add_medicine_dialog, null)
+            val medicineNameToAdd = dialogView.findViewById<EditText>(R.id.medicineNameToAdd)
+            val takeMorning = dialogView.findViewById<CheckBox>(R.id.morningCheck)
+            val takeLunch = dialogView.findViewById<CheckBox>(R.id.lunchCheck)
+            val takeDinner = dialogView.findViewById<CheckBox>(R.id.dinnerCheck)
+
+            builder.setView(dialogView)
+                .setPositiveButton("Add") { dialogInterface, i ->
+                    // Add Item to CustomListView
+                    (medicineList.adapter as MedicineListViewAdapter).add(Medicine(medicineNameToAdd.text.toString(),takeMorning.isChecked, takeLunch.isChecked, takeDinner.isChecked))
+                    (medicineList.adapter as MedicineListViewAdapter).notifyDataSetChanged()
+
+            }
+                .setNegativeButton("Cancel") {dialogInterface, i ->
+                    // Cancel Btn. Do nothing.
+                }
+                .show()
+
+
+        }
 
     }
 }
