579cc5a02ca6e024cfc2a7180c57e755802dd5ea
Taewan-P <swimingtw@gmail.com>
2020-03-19 14:37:41 +0900

    Add input name for ProfileActivity

--- app/src/main/java/com/threecsedevs/medichecker/ProfileActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/ProfileActivity.kt
@@ -1,11 +1,18 @@
 package com.threecsedevs.medichecker
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : Fragment() {
 
@@ -27,6 +34,39 @@ class ProfileActivity : Fragment() {
         var view = this.activity?.window?.decorView
         view!!.systemUiVisibility = view.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
 
-        
+        profileName.setOnClickListener {
+            val builder = AlertDialog.Builder(this.context)
+            val dialogView = layoutInflater.inflate(R.layout.activity_name_dialog, null)
+            val nameToAdd = dialogView.findViewById<EditText>(R.id.nameToAdd)
+
+            val addbtn = builder.setView(dialogView)
+                .setPositiveButton("Set") { dialogInterface, i ->
+                    profileName.text = nameToAdd.text
+                }
+                .setNegativeButton("Cancel") { dialogInterface, i ->
+                    // Do Nothing
+                }
+                .show()
+                .getButton(DialogInterface.BUTTON_POSITIVE)
+
+            addbtn.isEnabled = false
+            nameToAdd.addTextChangedListener(object: TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                    if (!TextUtils.isEmpty(p0.toString().trim())) {
+                        addbtn.isEnabled = true
+                    }
+                    else {
+                        nameToAdd.error = "Name is required."
+                        addbtn.isEnabled = false
+                    }
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            })
+
+
+        }
     }
 }
