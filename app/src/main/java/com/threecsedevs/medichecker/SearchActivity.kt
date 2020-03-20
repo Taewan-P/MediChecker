b932bc5963ad57dfb29258fd84ede5cf68fffe14
Taewan-P <swimingtw@gmail.com>
2020-03-20 18:55:58 +0900

    Remove unused imports

--- app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/SearchActivity.kt
@@ -3,22 +3,15 @@ package com.threecsedevs.medichecker
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_search.*
-import kotlinx.android.synthetic.main.activity_search_popup.*
-import kotlinx.coroutines.runBlocking
-import org.json.JSONArray
-import org.json.JSONObject
-import kotlin.concurrent.thread
 
 
 class SearchActivity : Fragment() {
