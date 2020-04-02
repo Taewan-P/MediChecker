4a43471d6e2a452d3e4fd778ac914c882435c5bf
Yeonggyun.Kim <kygha7205@naver.com>
2020-04-02 19:46:20 +0900

    Change http request module from Volley to Fuel and Fix searching performances of every API call

--- app/src/main/java/com/threecsedevs/medichecker/activities/InteractionResultActivity.kt
+++ app/src/main/java/com/threecsedevs/medichecker/activities/InteractionResultActivity.kt
@@ -3,10 +3,8 @@ package com.threecsedevs.medichecker.activities
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 import com.threecsedevs.medichecker.R
 import kotlinx.android.synthetic.main.activity_interaction_result.*
 import kotlinx.coroutines.*
@@ -21,8 +19,8 @@ class InteractionResultActivity : AppCompatActivity() {
         safe.visibility = View.INVISIBLE
         warning.visibility = View.INVISIBLE
         var count = 0
-        var result = mutableListOf<String>()
-        var queue = Volley.newRequestQueue(this)
+        var inter_result = mutableListOf<String>()
+//        var queue = Volley.newRequestQueue(this)
         val rxcuis = mutableListOf<String>()
         var drug = intent.getStringArrayListExtra("drugName")
 
@@ -30,9 +28,7 @@ class InteractionResultActivity : AppCompatActivity() {
             var job = async {
                 for (name in drug){
                     var drugId = getrxcuis(name)
-                    delay(2000L)
                     rxcuis.add(drugId)
-
                     count ++
 
                 }
@@ -45,31 +41,36 @@ class InteractionResultActivity : AppCompatActivity() {
 
             count ++
 
-            val getInteractionRequest = object :
-                StringRequest(Method.GET, interURL, Response.Listener { response ->
-                    val responseTest = JSONObject(response)
+            val (request, response, result) = interURL
+                .httpGet()
+                .responseString()
+
+            when (result) {
+                is Result.Failure -> {
+                    val ex = result.getException()
+                    println(ex)
+                }
+
+                is Result.Success -> {
+                    val data = result.get()
+                    val responseTest = JSONObject(data)
                     if ( responseTest.length() > 2 ) {//상호작용 결과가 있을때는 배열 길이가 3임.
                         val a: Any? = responseTest.getJSONArray("fullInteractionTypeGroup").get(0) //GET source from drugbank only
                         val b: Any? = JSONObject(a.toString()).get("fullInteractionType")
                         val inter_len: Int = JSONArray(b.toString()).length() //상호작용 Description 을담고있는 배열의 길이. For 문 반복횟수.
                         for ( i in 0 until inter_len ) {
-                            result.add(JSONObject(JSONObject(JSONArray(JSONObject(a.toString()).get("fullInteractionType").toString()).get(i).toString()).getJSONArray("interactionPair").get(0).toString()).get("description").toString())
+                            inter_result.add(JSONObject(JSONObject(JSONArray(JSONObject(a.toString()).get("fullInteractionType").toString()).get(i).toString()).getJSONArray("interactionPair").get(0).toString()).get("description").toString())
                         }
 
                     } else {
-                        result.add("Interaction between drugs not found")
+                        inter_result.add("Interaction between drugs not found")
                     }
                     count ++
-                }, Response.ErrorListener { error ->
-                }) {
-                override fun getBodyContentType(): String {
-                    return "application/json; charset=utf-8"
                 }
             }
-            queue.add(getInteractionRequest)
-            delay(3000L)
+
             runOnUiThread {
-                changeResultText(result)
+                changeResultText(inter_result)
                 count ++
             }
         }
@@ -111,35 +112,30 @@ class InteractionResultActivity : AppCompatActivity() {
     }
 
     fun getrxcuis(s: String): String{
-        var queue = Volley.newRequestQueue(this)
         val url = makeRxcuiURL(s)
-        var result: String
-        result = ""
-        val getRXCUIRequest = object : StringRequest(Request.Method.GET, url, Response.Listener { response ->
-            runBlocking {
-                var job = launch {
-                    val responseTest = JSONObject(response)
-                    responseTest.getJSONObject("idGroup").getJSONArray("rxnormId").get(0)
-                            .apply {
-                                result = toString()
-                            }
-                }
-                job.join()
+        var data: String = ""
+
+
+        val (request, response, result) = url
+            .httpGet()
+            .responseString()
+
+        when (result) {
+            is Result.Failure -> {
+                val ex = result.getException()
+                println(ex)
             }
 
-        }, Response.ErrorListener { error ->
-            println("Response Unsuccessful : $error") //This line should not be delete for debugging error
-            result = "Server Response error. Please try later."
-        }) {
-            override fun getBodyContentType(): String {
-                return "application/json; charset=utf-8"
+            is Result.Success -> {
+
+                data = JSONObject(result.get()).getJSONObject("idGroup").getJSONArray("rxnormId").get(0).toString()
+
+                println(data)
+                return data
             }
         }
 
-        queue.add(getRXCUIRequest)
-        runBlocking {
-            delay(3000L)
-        }
-        return result
+
+        return data
     }
 }
