8aadd9a81d73ca7d198ce289948afc37ca98bb29
mjkya <mjkya@naver.com>
2020-04-06 22:29:31 +0900

    update input med capital

--- app/src/main/java/com/threecsedevs/medichecker/input_medicine/InputMedicineListViewAdapter.kt
+++ app/src/main/java/com/threecsedevs/medichecker/input_medicine/InputMedicineListViewAdapter.kt
@@ -11,8 +11,6 @@ import android.widget.Button
 import android.widget.Toast
 import com.threecsedevs.medichecker.R
 import com.threecsedevs.medichecker.activities.SearchAllActivity
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
 class InputMedicineListViewAdapter(context: Context, var resource: Int, var items: MutableList<String>) : ArrayAdapter<String>(context, resource, items){
     var selectedIdx : Int = -1
@@ -26,7 +24,6 @@ class InputMedicineListViewAdapter(context: Context, var resource: Int, var item
         var drugName = items[position]
         if (drugName == "") {
             inputBtn.text = context.getString(R.string.input_medicine)
-            inputBtn.setAllCaps(false);
         }
         else {
             inputBtn.text = drugName
